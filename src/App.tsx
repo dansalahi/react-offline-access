0d9bf1a6d0be5081aac0b2a074d3d8bfff247ff6
daniel salahi <dan.salahii@gmail.com>
2023-05-31 12:05:30 +0300

    disabled react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Posts } from './components/posts'
+// import { QueryClient, QueryClientProvider } from 'react-query'
+// import { ReactQueryDevtools } from 'react-query/devtools'
+// const queryClient = new QueryClient({
+//   defaultOptions: {
+//     queries: {
+//       staleTime: 60 * 1000 * 5,
+//       retry: 3,
+//       retryDelay: 5000
+//     }
+//   }
+// })
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <QueryClientProvider client={queryClient}>
+    <div className='App'>
+      <h1>React APP</h1>
+      <h2 style={{ color: `${navigator.onLine ? 'green' : 'red'}` }}>{navigator.onLine ? 'ONLINE' : 'OFFLINE'}</h2>
+      <Posts />
     </div>
-  );
+    // <ReactQueryDevtools initialIsOpen />
+    // </QueryClientProvider>
+  )
 }
 
-export default App;
+export default App
